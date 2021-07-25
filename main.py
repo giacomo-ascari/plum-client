e6569b378e568deb0b9541905365b46666da67f8
giacomo-ascari <giacomolego@gmail.com>
2021-07-25 12:38:02 +0200

    feat: should be all

--- main.py
+++ main.py
@@ -1,57 +1,61 @@
 import os
 import sys
-import platform
-import psutil
+import paho.mqtt.client as mqtt
 from dotenv import load_dotenv
+import psutil
+import time
+import json
+
 load_dotenv()
 
-PORT = os.getenv("PORT")
+NAME = os.getenv("NAME")
+BROKER = os.getenv("BROKER")
+PORT = int(os.getenv("PORT"))
+KEEPALIVE = int(os.getenv("KEEPALIVE"))
+TOPIC = os.getenv("TOPIC")
+INTERVAL = int(os.getenv("INTERVAL"))
 
-def cpu_stats():
-    psutil.cpu_percent()
-    sleep(0.2)
-    cpu_percent = psutil.cpu_percent()
-    cpu_freqs = psutil.cpu_freq(percpu=True)
-    avg_current = sum(x.current / len(cpu_freqs) for x in cpu_freqs)
-    s = "<b>CPU</b>\n"
-    s += f"{bar(cpu_percent, len=12)}\n"
-    s += f"{psutil.cpu_count()} cores - {avg_current}Hz - {cpu_percent}%\n"
-    return s
+def on_connect(client, userdata, rc, what):
+    print("Connected.", userdata, rc, what)
+    client.subscribe(TOPIC)
 
-def mem_stats():
-    mem = psutil.virtual_memory()
-    s = "<b>MEMORY</b>\n"
-    s += f"{bar(mem.percent, 12)}\n"
-    s += f"{hrs(mem.total)} tot - {hrs(mem.used)} used\n"
-    return s
+def on_message(client, userdata, msg):
+    print("Message received.", userdata, msg.payload.decode("utf-8"), msg.topic, msg.qos, msg.retain)
 
-def disk_stats():
-    s = "<b>DISKS</b>\n"
-    if sys.platform=="win32":
-        for disk in psutil.disk_partitions():
-            usage = psutil.disk_usage(disk.device)
-            s += f"{bar(usage.percent, 12)}\n"
-            s += f"{disk.device}: {hrs(usage.total)} tot - {hrs(usage.used)} used\n"
-    else:
-        usage = psutil.disk_usage("/")
-        s += f"{bar(usage.percent, 12)}\n"
-        s += f"/: {hrs(usage.total)} tot - {hrs(usage.used)} used\n"
-    return s
+def on_disconnect(client, userdata, rc):
+    print("Disconnected.")
 
-def sens_stats():
-    s = "<b>SENSORS</b>\n"
-    if sys.platform=="win32":
-        s += "not available on windows\n"
-    else:
-        for name, entries in psutil.sensors_temperatures().items():
-            s += f" {name}:\n"
-            for entry in entries:
-                s += f"{bar(entry.current, 12)}\n"
-                s += f"{entry.current} °C, high:{entry.high!=None}, crit:{entry.critical!=None}\n"
-    return s
+def publish(client):
+    data = {}
+    data["cpu_percent"] = psutil.cpu_percent(interval=1) # val
+    data["cpu_freq"] = psutil.cpu_freq() # current min max
+    data["virtual_memory"] = psutil.virtual_memory() # total available percent used free
+    data["swap_memory"] = psutil.swap_memory() # total used free percent sin sout
+    data["disk_usage"] = psutil.disk_usage('/') # total used free percent
+    data["net_io_counters"] = psutil.net_io_counters() #bytes_sent bytes_recv packets_sent packets_recv errin errout dropin dropout
+    data["boot_time"] = psutil.boot_time() # val
+    if sys.platform!="win32":
+        data["sensors_temperatures"] = psutil.sensors_temperatures()
+    client.publish(TOPIC, json.dumps(data))
 
 def main():
-    print(PORT)
+    try:
+        client = mqtt.Client(NAME)
+        client.on_connect = on_connect
+        #client.on_message = on_message
+        client.on_disconnect = on_disconnect
+        client.connect(BROKER, PORT, KEEPALIVE)
+        client.loop_start()
+        while True:
+            publish(client)
+            time.sleep(INTERVAL)
+    except KeyboardInterrupt:
+        print("KEYBOARD INTERRUPT")
+    except Exception as e:
+        print("OTHER EXCEPTION:", e)
+    finally:
+        client.loop_stop()
+        client.disconnect()
 
 if __name__ == "__main__":
     main()
